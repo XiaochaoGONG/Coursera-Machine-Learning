9ad7a5012a31ee7203fdd63a8207e08902c0531e
XiaochaoGONG <gxc_seu@126.com>
2017-11-03 23:27:20 +0800

    finish ex2

--- notebooks/Ex2_logistic.py
+++ notebooks/Ex2_logistic.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from Logistic_Regression import *
+from scipy.optimize import minimize
 import pdb
 
 def bp():
@@ -10,6 +11,20 @@ def plotData(X, y):
     neg_idx = [idx for (idx, val) in enumerate(y[:]) if (val == 0)]
     plt.scatter(X[pos_idx, 0], X[pos_idx, 1], c = 'r', marker = '+') 
     plt.scatter(X[neg_idx, 0], X[neg_idx, 1], c = 'y', marker = 'o')
+
+def plotDecisionBoundary(theta, X, y):
+    x1_min, x1_max = min(X[:, 1]), max(X[:, 1])
+    x2_min, x2_max = min(X[:, 2]), max(X[:, 2])
+
+    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max), np.linspace(x2_min, x2_max))
+    xx = np.c_[np.ones([xx1.ravel().shape[0], 1]), xx1.ravel(), xx2.ravel()]
+
+    z = xx.dot(theta.reshape(-1, 1))
+    h = sigmoid(z)
+    h = h.reshape(xx1.shape)
+    plt.contour(xx1, xx2, h, [0.5], linewidths = 1, colors = 'b')
+
+
 
 if __name__ == '__main__':
     data = np.loadtxt('data/ex2data1.txt', delimiter = ',')
@@ -30,8 +45,8 @@ if __name__ == '__main__':
     plotData(X, y)
     plt.xlabel('Exam 1 score')
     plt.ylabel('Exam 2 score')
-    plt.legend(['Admitted', 'Not admitted'], loc = 'best')
-    #plt.show()
+    leg = ['Admitted', 'Not admitted']
+    plt.legend(leg, loc = 'best')
 
 
     #########################################
@@ -43,25 +58,50 @@ if __name__ == '__main__':
 
     initial_theta = np.zeros([num_Features + 1, 1])
 
-    cost, grad = costFunction(initial_theta, X, y)
+    cost = costFunction(initial_theta, X, y)
+    grad = gradientFunction(initial_theta, X, y)
     print 'Cost at initial theta (zeros): %f' % cost
-    print 'Expected cost (approx): 0.693'
+    print 'Expected cost (approx): 0.693\n'
     print 'Gradient at initial theta (zeros):'
     print grad
     print 'Expected gradients (approx):'
-    print ' -0.1000\n -12.0092\n -11.2628'
+    print ' -0.1000\t-12.0092\t-11.2628\n'
 
     test_theta = np.c_[[-24, 0.2, 0.2]]
-    cost, grad = costFunction(test_theta, X, y)
+    cost = costFunction(test_theta, X, y)
+    grad = gradientFunction(test_theta, X, y)
     print 'Cost at initial theta (zeros): %f' % cost
-    print 'Expected cost (approx): 0.218'
+    print 'Expected cost (approx): 0.218\n'
     print 'Gradient at initial theta (zeros):'
     print grad
     print 'Expected gradients (approx):'
-    print ' 0.043\n 2.566\n 2.647'
-
+    print ' 0.043\t2.566\t2.647\n'
 
     #########################################
-    ##  3. 
+    ##  3. Optimizing
     ##  2017.11.03
     #########################################
+    res = minimize(costFunction, initial_theta, args = (X, y), method = None, jac = gradientFunction, options = {'maxiter' : 400})
+    print res.message
+    if res.status == 0:
+        # Success
+        theta = res.x.reshape(-1, 1)
+        print 'Theta : '
+        print theta
+
+
+    #########################################
+    ##  4. Predict and Accuracies
+    ##  2017.11.03
+    #########################################
+    X_test = np.c_[1, 45, 85]
+    prob = sigmoid(X_test.dot(theta))
+    print 'For a student with score 45 and 85, we predict an admission probability of %f\n' % prob
+    plt.scatter(X_test[:, 1], X_test[:, 2], marker='v')
+    leg.append('(45, 85)')
+    plt.legend(leg, loc = 'best')
+    plotDecisionBoundary(theta, X, y)
+    plt.show()
+    
+    p = predict(theta, X)
+    print 'Train Accuracy: %f' % np.mean(p == y)
